af420d7c4a51fe99ba80bd279cd295bd86fb2dd6
unknown <tariqmuhanna14@gmail.com>
2022-05-14 22:19:34 -0500

    Completed note keeper app, added add note, delete note, and separated components

---
+++ note-keeper-app/src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((item, index) => {
+        return (
+          <Note 
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
